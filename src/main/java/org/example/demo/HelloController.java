828d10c959ea54138889a70267b38412582536c8
ZeroPunq <davidironhell@gmail.com>
2025-01-28 12:12:15 +0100

    Proyecto corregido. Interfaz más atractiva, añadidos dos parametros para estadisticas.
    Commit final

--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -19,16 +19,40 @@ public class HelloController {
     @FXML
     private ComboBox<String> reportComboBox;
     @FXML
+    private ComboBox<String> teamComboBox;
+    @FXML
     private Button generateReportButton;
     @FXML
     private TextField seasonTextField;
+
     @FXML
     public void initialize() {
-        // Opciones del ComboBox
-        reportComboBox.getItems().addAll("Estadísticas jugadores por temporada", "Jugadores en equipo");
-        reportComboBox.getSelectionModel().selectFirst(); // Selección predeterminada
+        // Opciones del ComboBox de Reportes
+        reportComboBox.getItems().addAll(
+                "Estadísticas jugadores por temporada",
+                "Jugadores en equipo"
+        );
+        reportComboBox.getSelectionModel().selectFirst();
 
+        // Opciones del ComboBox de Equipos
+        teamComboBox.getItems().addAll(
+                "76ers", "Bobcats", "Bucks", "Bulls", "Cavaliers", "Celtics",
+                "Clippers", "Grizzlies", "Hawks", "Heat", "Hornets", "Jazz",
+                "Kings", "Knicks", "Lakers", "Magic", "Mavericks", "Nets",
+                "Nuggets", "Pacers", "Pistons", "Raptors", "Rockets", "Spurs", "Suns"
+        );
+        teamComboBox.getSelectionModel().selectFirst();
+
+        // Listener para ocultar el campo de temporada si se selecciona "Jugadores en equipo"
+        reportComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if ("Jugadores en equipo".equals(newValue)) {
+                seasonTextField.setVisible(false);
+            } else {
+                seasonTextField.setVisible(true);
+            }
+        });
     }
+
     @FXML
     public void generateReport() {
         try{
@@ -37,9 +61,10 @@ public class HelloController {
 
             Map parametros = new HashMap();
             parametros.put("temporada", seasonTextField.getText());
+            parametros.put("nombre_equipo",teamComboBox.getSelectionModel().getSelectedItem());
             if(reportComboBox.getSelectionModel().getSelectedItem().equals("Estadísticas jugadores por temporada")) {
 
-                JasperPrint print = JasperFillManager.fillReport("/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_estadisticas_jugadores_def.jasper", parametros, conexion);
+                JasperPrint print = JasperFillManager.fillReport("/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_estadisticas_jugadores_def_2.jasper", parametros, conexion);
                 JasperExportManager.exportReportToPdfFile(print, "/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_estadisticas_jugadores.pdf");
             }else if (reportComboBox.getSelectionModel().getSelectedItem().equals("Jugadores en equipo")) {
                 JasperPrint print = JasperFillManager.fillReport("/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_jugadores_equipos.jasper", parametros, conexion);
