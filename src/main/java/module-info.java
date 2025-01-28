e54b25ba10ddc90929efabd1090a16405ac8a576
ZeroPunq <davidironhell@gmail.com>
2025-01-28 11:23:27 +0100

    Proyecto corregido, saca un pdf correcto pero otro en blanco

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
+
+
+    opens org.example.demo to javafx.fxml;
+    exports org.example.demo;
+}
---
+++ src/main/java/org/example/demo/HelloController.java
@@ -0,0 +1,52 @@
+package org.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloController {
+    @FXML
+    private ComboBox<String> reportComboBox;
+    @FXML
+    private Button generateReportButton;
+    @FXML
+    private TextField seasonTextField;
+    @FXML
+    public void initialize() {
+        // Opciones del ComboBox
+        reportComboBox.getItems().addAll("Estadísticas jugadores por temporada", "Jugadores en equipo");
+        reportComboBox.getSelectionModel().selectFirst(); // Selección predeterminada
+
+    }
+    @FXML
+    public void generateReport() {
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/nba","root","");
+
+            Map parametros = new HashMap();
+            parametros.put("Estado",reportComboBox.getSelectionModel().getSelectedItem());
+            if(reportComboBox.getSelectionModel().getSelectedItem().equals("Estadísticas jugadores por temporada")) {
+                parametros.put("Temporada", seasonTextField.getText());
+                JasperPrint print = JasperFillManager.fillReport("/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_estadisticas_jugadores_2.jasper", parametros, conexion);
+                JasperExportManager.exportReportToPdfFile(print, "/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_estadisticas_jugadores.pdf");
+            }else if (reportComboBox.getSelectionModel().getSelectedItem().equals("Jugadores en equipo")) {
+                JasperPrint print = JasperFillManager.fillReport("/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_jugadores_equipos.jasper", parametros, conexion);
+                JasperExportManager.exportReportToPdfFile(print, "/media/alumno/3EF5-1E86/DAM/Segundo/Interfaces/UT5/Informe2Def/demo/src/Informes/nba_jugadores_equipos.pdf");
+            }
+        }catch (Throwable e){
+            e.printStackTrace();
+        }
+    }
+}
